7aedbd47cd6962e5af6635c3454476b2b602b955
Mikhail Kovbasenko <hankchinaski34@gmail.com>
2021-08-26 11:30:06 +0300

    update hashrouter-3

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import store from './redux/redux-store';
 
 ReactDOM.render(
 	<React.StrictMode>
-		<HashRouter basename={'/admin'}>
+		<HashRouter>
 			<Provider store={store}>
 				<App />
 			</Provider>
