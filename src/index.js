15ce0d7e8e9c5762f50aba7d40ab71c85a9d667e
Mikhail Kovbasenko <hankchinaski34@gmail.com>
2021-08-26 10:40:11 +0300

    update hashrouter

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import store from './redux/redux-store';
 
 ReactDOM.render(
 	<React.StrictMode>
-		<HashRouter>
+		<HashRouter basename={'https://mikhail-kovbasenko.github.io/car-sharing-admin/'}>
 			<Provider store={store}>
 				<App />
 			</Provider>
